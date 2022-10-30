60d85f31135004a0b522dde0f72d3953248ce69e
Jorge Seu Churrasco com <eusoulegaldemaiseverdadeeujuro@gmail.com>
2022-10-30 19:44:54 -0300

    improved jChunkExploit
    detects when exploit occur
    better chunk rendering
    
    need some otimizations

--- src/main/java/meteordevelopment/meteorclient/systems/modules/JJhack/JChunkTests.java
+++ src/main/java/meteordevelopment/meteorclient/systems/modules/JJhack/JChunkTests.java
@@ -5,13 +5,26 @@
 
 package meteordevelopment.meteorclient.systems.modules.JJhack;
 
-import baritone.api.event.events.ChunkEvent;
 import meteordevelopment.meteorclient.events.render.Render3DEvent;
-import meteordevelopment.meteorclient.renderer.ShapeMode;
+import meteordevelopment.meteorclient.events.world.TickEvent;
+import meteordevelopment.meteorclient.settings.BoolSetting;
+import meteordevelopment.meteorclient.settings.IntSetting;
+import meteordevelopment.meteorclient.settings.Setting;
+import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.systems.modules.Categories;
 import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.meteorclient.systems.modules.Modules;
 import meteordevelopment.meteorclient.utils.render.color.Color;
 import meteordevelopment.orbit.EventHandler;
+import net.minecraft.client.sound.Sound;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
+
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+import static java.lang.Math.*;
 
 public class JChunkTests extends Module{
 
@@ -19,17 +32,143 @@ public class JChunkTests extends Module{
       super(Categories.JJHACK, "JChunkExploit", "Testing");
    }
 
+   private final SettingGroup sgVars = settings.createGroup("variaveis");
+
+   private final Setting<Integer> x = sgVars.add(new IntSetting.Builder()
+           .name("X")
+           .description("aaa")
+                   .defaultValue(128)
+                   .sliderMax(300)
+           .build()
+   );
+   private final Setting<Integer> z = sgVars.add(new IntSetting.Builder()
+           .name("z")
+           .description("aaa")
+           .defaultValue(128)
+
+           .sliderMax(300)
+           .build()
+   );
+   private final Setting<Boolean> logOnChat = sgVars.add(new BoolSetting.Builder()
+           .name("log")
+           .description("aaa")
+           .defaultValue(false)
+           .build()
+   );
+   private final Setting<Integer> x2 = sgVars.add(new IntSetting.Builder()
+           .name("X2")
+           .description("aaa")
+           .defaultValue(128)
+
+           .sliderMax(300)
+           .build()
+   );
+   private final Setting<Integer> z2 = sgVars.add(new IntSetting.Builder()
+           .name("z2")
+           .description("aaa")
+           .defaultValue(128)
+           .sliderMax(300)
+           .build()
+   );
+
+   private final Setting<Integer> excludeSmth = sgVars.add(new IntSetting.Builder()
+           .name("excludeDir")
+           .description("aaa")
+           .defaultValue(1)
+           .sliderMax(8)
+           .build()
+   );
+
+   private final ArrayList<JChunks> loadedChunks = new ArrayList<>();
 
    @EventHandler
-   private void onChunkLoad(ChunkEvent chunkEvent, Render3DEvent render3DEvent){
-         render3DEvent.renderer.box(chunkEvent.getX(), 2, chunkEvent.getZ(), chunkEvent.getX() + 10, 4, chunkEvent.getZ() + 10 ,
-              Color.BLUE ,Color.RED, ShapeMode.Both, 1);
-      System.out.println(chunkEvent.getX());
+   private void onRender(Render3DEvent render3DEvent){
+      for(JChunks JChunks : loadedChunks){
+         render(render3DEvent, JChunks);
+      }
+   }
+   private void render(Render3DEvent event, JChunks chunk){
+      event.renderer.boxLines(chunk.getX(), 0, chunk.getZ(), chunk.getX2(),0,chunk.getZ2(), Color.RED, excludeSmth.get());
+   }
+   private JChunks chunk;
+   @EventHandler
+   private void onWalk(TickEvent.Post event){
+
+       chunk = new JChunks(mc.player.getChunkPos().getStartX(), mc.player.getChunkPos().getStartZ());
+
+       //handle null
+      if(loadedChunks.isEmpty()) {
+         chunk.addChunk();
+      }
+
+      final JChunks lastChunk = loadedChunks.get(loadedChunks.size() - 1);
+      if(abs(chunk.getX() - lastChunk.getX()) > 15.9
+         || abs(chunk.getZ() - lastChunk.getZ()) > 15.9){
+            chunk.addChunk();
+         }
+
    }
 
-   private void render(Render3DEvent event, ChunkEvent chunkEvent){
-      event.renderer.box(chunkEvent.getX(), 2, chunkEvent.getZ(), chunkEvent.getX() + 10, 4, chunkEvent.getZ() + 10 ,
-              Color.BLUE ,Color.RED, ShapeMode.Both, 1);
+   private boolean hasChanged = false;
+   private boolean hasChangedCopy = false;
+   private long timeNew;
+   private long timeOld;
+
+
+   private String lastBar = "";
+
+   @EventHandler
+   private void onExploitOccur(TickEvent.Pre event) {
+      String actionBar = Modules.get().getActionBar();
+      if (actionBar == null || lastBar == null || actionBar.length() < 18) return;
+
+      if (actionBar.charAt(18) == 'N' || actionBar.charAt(18) == 'O') {
+         if (actionBar.equals(lastBar)) {
+            timeOld = System.currentTimeMillis();
+            hasChanged = false;
+         } else {
+            timeNew = System.currentTimeMillis();
+            hasChanged = true;
+         }
+         lastBar = actionBar;
+      }
+      if(hasChanged){
+         if(timeNew - timeOld < 80){
+            if(logOnChat.get()) System.out.println(timeNew - timeOld);
+            mc.player.playSound(SoundEvents.ENTITY_GENERIC_EXPLODE, 21, 21);
+         }
+      }
+   }
+
+   private class JChunks {
+     private int x, z;
+
+     public void addChunk(){
+        loadedChunks.add(this);
+     }
+      public JChunks(int x, int z){
+         setX(x);
+         setZ(z);
+      }
+      public int getX() {
+         return this.x;
+      }
+      public void setX(int x) {
+         this.x = x + 128;
+      }
+      public int getZ() {
+         return this.z;
+      }
+      public void setZ(int z) {
+         this.z = z + 128;
+      }
+      public double getX2() {
+         return x + 144;
+      }
+      public double getZ2() {
+         return z + 144;
+      }
+
    }
 
 }
